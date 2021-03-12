ad1fa6476a0ec1b58e1147594a8f1f398789b89b
mavaquero05 <mavaquero05@iesalbarregas.es>
2021-03-12 19:24:27 +0100

    Primera validación

---
+++ miguelAngel_VaqueroDAM/src/miguelAngel_VaqueroDAM/Prueba1.java
@@ -0,0 +1,11 @@
+package miguelAngel_VaqueroDAM;
+
+public class Prueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola Git");
+	}
+
+}
